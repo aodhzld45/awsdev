9bed4feaa4a452e13141c05c89442d6aefcf75cd
서현석 <71122744+aodhzld45@users.noreply.github.com>
2025-01-02 11:02:38 +0900

    aws 테스트 git

---
+++ src/main/java/com/awsdev/spb/awsdev/AwsdevApplication.java
@@ -0,0 +1,13 @@
+package com.awsdev.spb.awsdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awsdev/spb/awsdev/AwsdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsdev.spb.awsdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
